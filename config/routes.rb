9a91449bc2f5ab1607776d6a6771838625c04f46
jrdnndtsch <jordanbdeutsch@gmail.com>
2017-03-13 15:08:44 -0400

    update devise controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :activities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'activities#index'
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 end
